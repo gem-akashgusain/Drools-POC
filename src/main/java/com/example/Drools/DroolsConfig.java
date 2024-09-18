6b0f87310aaad0434439f96d7101cacf485a04f9
akash.gusain <akash.gusain@geminisolutions.com>
2024-09-18 11:11:26 +0530

    Drools using Rules in excel file.

--- src/main/java/com/example/Drools/DroolsConfig.java
+++ src/main/java/com/example/Drools/DroolsConfig.java
@@ -4,6 +4,7 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieModule;
+import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.internal.io.ResourceFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,14 +12,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DroolsConfig {
-
-    private static final String RULES_CUSTOMER_RULES_DRL = "rules/customer-discount.drl";
+    private static final String RULES_ORDER_DISCOUNT_XLS = "rules/customer-rules.xlsx";
     private static final KieServices kieServices = KieServices.Factory.get();
-
     @Bean
     public KieContainer kieContainer() {
-        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newClassPathResource(RULES_CUSTOMER_RULES_DRL));
+        Resource dt = ResourceFactory.newClassPathResource(RULES_ORDER_DISCOUNT_XLS, getClass());
+        KieFileSystem kieFileSystem = kieServices.newKieFileSystem().write(dt);
         KieBuilder kb = kieServices.newKieBuilder(kieFileSystem);
         kb.buildAll();
         KieModule kieModule = kb.getKieModule();
